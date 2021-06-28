21d3fc0739965753d062638444638048e6452552
AnSt <starininandrey@gmail.com>
2021-06-28 15:46:05 +0300

    Update

--- LINQ_Test/Program.cs
+++ LINQ_Test/Program.cs
@@ -17,10 +17,14 @@ namespace LINQ_Test
             Console.WriteLine($"min = {min}");
             Console.WriteLine($"max = {max}");
 
-            var select = from item in list 
+            var select1 = from item in list 
                                         where item > 5 
                                         select item;
-            PrintList(select);
+            //var select2 = list.Where(item => item is < 5 and > 1);
+            var select2 = list.Where(item => item < 5 && item > 1).Where(i => i % 2 == 0);
+            PrintList(select1);
+            PrintList(select2);
+            
         }
 
         static void RandomList(out List<int> list, in int size, in int start, in int end)
